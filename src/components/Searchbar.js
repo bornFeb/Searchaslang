6a740c84c0c8526db0e5021ceaef9296f93362d1
harshdevl <harshbuddhdev5@gmail.com>
2023-01-06 13:31:13 -0500

    search functionality 50%

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -6,36 +6,9 @@ import Results from './Results.js';
 export default function Searchbar(){
   
 
-  // const slangs = [
-  //   {
-  //     word: "abdabs",
-  //     meaning: "Noun. Terror, the frights, nerves. Often heard as the screaming abdabs. Also very occasionally 'habdabs'. [1940s]"
-  //   },
-  //   {
-  //     word: "ablute",
-  //     meaning: "nai janvu"
-  //   },
-  //   {
-  //     word: "absobloodylutely",
-  //     meaning: "nai khabar"
-  //   },
-  //   {
-  //     word: "shiva",
-  //     meaning: "angery lord/aghori"
-  //   },
-  //   {
-  //     word: "vishnu",
-  //     meaning: "chill lord"
-  //   },
-  //   {
-  //     word: "bhrmha",
-  //     meaning: "big lord"
-  //   }
-  // ]
-
+ 
 
   const[text,setText] = useState('');
-  // const [filteredList, setFilteredList] = useState(slangs);
 
   const onChangeHandler = (event) =>{
     // console.log(obj);
@@ -49,7 +22,7 @@ export default function Searchbar(){
       <input className='searchBox'  placeholder="Search a slang" onChange={onChangeHandler}  value={text} />
       <ul className="list-group" style={{maxHeight: 400, overflow: 'auto'}}>  
                   <li style={{backgroundColor: "white", textAlign: "left", paddingLeft: "8px", borderRadius:"10px"}} >
-                    <Results></Results>
+                    <Results ans={text}/>
                   </li>
                 </ul>
     </div>
