c159208651d3f79bea4333b691b4f4167f3dc068
Harsh Buddhdev <harshbuddhdev5@.com>
2023-09-30 02:28:59 -0400

    random background color effect added.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import './App.css';
 import Searchbar from './components/Searchbar.js';
+import React, { useEffect, useState } from 'react';
+
 
 
 function App() {
 
-  // Initialize Firebase
+  const [background, setBackground] = useState('');
+  const generateRandomGradient = () => {
+    const colors = [
+      getRandomColor(),
+      getRandomColor(),
+      getRandomColor(),
+      getRandomColor(),
+    ];
+    const gradientDirection = `${getRandomDirection()}deg`;
+
+    return `linear-gradient(${gradientDirection}, ${colors.join(', ')})`;
+  };
+  const getRandomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+  const getRandomDirection = () => {
+    const directions = ['0', '45', '90', '135', '180', '225', '270', '315'];
+    return directions[Math.floor(Math.random() * directions.length)];
+  };
+  useEffect(() => {
+    const randomGradient = generateRandomGradient();
+    setBackground(randomGradient);
+  }, []);
+
   return (
-    <div className="App">
-    <Searchbar></Searchbar>
+    <div className="App"
+      style={{ background }}
+    >
+      <Searchbar></Searchbar>
     </div>
   );
 }
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,31 +1,28 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './Searchbar.css';
 import MyProfile from './MyProfile';
 import Results from './Results.js';
 
-export default function Searchbar(){
-  
+export default function Searchbar() {
 
- 
+  const [text, setText] = useState('');
 
-  const[text,setText] = useState('');
-
-  const onChangeHandler = (event) =>{
+  const onChangeHandler = (event) => {
     // console.log(obj);
     setText(event.target.value);
     //search from database function will reside here
-  } 
-  return(
-             <> <MyProfile></MyProfile>
-    <div className='searchBar'>
+  }
+  return (
+    <> <MyProfile></MyProfile>
+      <div className='searchBar'>
 
-      <input className='searchBox'  placeholder="Search a slang" onChange={onChangeHandler}  value={text} />
-      <ul className="list-group" style={{maxHeight: 400, overflow: 'auto'}}>  
-                  <li style={{backgroundColor: "white", textAlign: "left", paddingLeft: "8px", borderRadius:"10px"}} >
-                    <Results ans={text}/>
-                  </li>
-                </ul>
-    </div>
+        <input className='searchBox' placeholder="Search a slang" onChange={onChangeHandler} value={text} />
+        <ul className="list-group" style={{ maxHeight: 400, overflow: 'auto' }}>
+          <li style={{ backgroundColor: "white", textAlign: "left", paddingLeft: "8px", borderRadius: "10px" }} >
+            <Results ans={text} />
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
